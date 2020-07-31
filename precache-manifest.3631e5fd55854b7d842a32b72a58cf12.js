57c476faa0e3be2ff23a75b51428a827a03c0ec2
konkazazis <nikos_kazazis@yahoo.com>
2020-07-31 20:34:04 +0300

    Updates

---
+++ precache-manifest.3631e5fd55854b7d842a32b72a58cf12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf0a8606dbcf40249e0782fa27e0cdf",
+    "url": "/reactRavenous/index.html"
+  },
+  {
+    "revision": "0aa07d8e93240bfa7ce2",
+    "url": "/reactRavenous/static/css/main.bab3b442.chunk.css"
+  },
+  {
+    "revision": "b0bc859f895acf212bf5",
+    "url": "/reactRavenous/static/js/2.0382a6f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactRavenous/static/js/2.0382a6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa07d8e93240bfa7ce2",
+    "url": "/reactRavenous/static/js/main.ef4233dd.chunk.js"
+  },
+  {
+    "revision": "dde0948e6208d1d27dc4",
+    "url": "/reactRavenous/static/js/runtime-main.7f110f32.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/reactRavenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/reactRavenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
